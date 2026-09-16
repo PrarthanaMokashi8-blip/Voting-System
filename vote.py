4785b4ed0381182fec5757f42490a125ef8bd4a2
PrarthanaMokashi8-blip <prarthanamokashi8@gmail.com>
2026-09-16 08:51:30 +0530

    Correct voting eligibility check and add greeting
    Fix syntax errors and add welcome message.

--- vote.py
+++ vote.py
@@ -1,7 +1,9 @@
 age=int(input("Enter your age");
+print("Welcome to voting system");
 if age>=18
     print("You are eligible to vote!")
     print("You can register as a voter")
 else:
     print("You are not eligible to vote!")
     print("You must be atleast 18 years old")
+    
